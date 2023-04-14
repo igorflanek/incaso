7415005ca7c2d757cffcf5b3a350e3ccd58ebd2c
igorflanek <55188779+igorflanek@users.noreply.github.com>
2023-04-14 13:08:30 +0200

    Add files via upload

---
+++ Incaso pierwsze testy/Object Repository/Page_Skaner iAML/th_Andrzej Duda.rs
@@ -0,0 +1,99 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>th_Andrzej Duda</name>
+   <tag></tag>
+   <elementGuidId>6c9d2b42-d37f-4e00-98f9-0ee2a17e96cb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>th</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Imię i nazwisko'])[1]/following::th[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>th</value>
+      <webElementGuid>d5375e15-c4d7-4c2f-ac1c-04b189c4123d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Andrzej Duda</value>
+      <webElementGuid>e31e8b1a-9789-46db-9a6c-7cd1c1de5863</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;d-flex flex-column min-vh-100&quot;]/main[1]/section[@class=&quot;pt0&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-12&quot;]/table[@class=&quot;table table-responsive-sm table-striped table-hover&quot;]/tbody[1]/tr[1]/td[1]/table[@class=&quot;table table-responsive-sm table-striped table-hover&quot;]/tbody[1]/tr[1]/th[1]</value>
+      <webElementGuid>0e8b691e-fb27-4229-83a3-1a6947af7b65</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Imię i nazwisko'])[1]/following::th[1]</value>
+      <webElementGuid>72558db6-5502-4928-b4e4-654eeb31b0b5</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Lista PEP: 2'])[1]/following::th[1]</value>
+      <webElementGuid>bf1a2381-b0aa-4700-ab57-cfa2bd9406ec</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Obywatelstwo i płeć'])[1]/preceding::th[1]</value>
+      <webElementGuid>f1c10131-38f1-45e3-840e-0a1ed38ccb41</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Poland (m)'])[1]/preceding::th[1]</value>
+      <webElementGuid>0b1b8c62-2be3-4c18-b972-fe1fa931c84e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Andrzej Duda']/parent::*</value>
+      <webElementGuid>3ceae195-3931-4cd4-9f61-7b22c67e1b5c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//th</value>
+      <webElementGuid>ff6e4c5f-7eec-4991-8cda-c202964a684b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//th[(text() = 'Andrzej Duda' or . = 'Andrzej Duda')]</value>
+      <webElementGuid>7a87454e-8b3a-4720-bf63-196062326dd5</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
